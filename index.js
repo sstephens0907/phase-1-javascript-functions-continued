0258113489aa73fe09329882fe25ebce83f43e85
Sarah Stephens <sarahstephens84@gmail.com>
2022-09-23 17:42:53 -0400

    commit tree eba478d12bb348c4154436a27c60d508768d4652

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate" ){
+    return `This Saturday, I want to ${activity}!`
+}
+saturdayFun("bike ride")
+
+function mondayWork(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`
+}
+
+function wrapAdjective(flair = "*", msg = "You are a ") {
+    const innerFunction = function (describer = "special"){
+        return `${msg} ${flair} ${describer} ${flair}!`;
+    };
+    return innerFunction ("","");
+}
+
